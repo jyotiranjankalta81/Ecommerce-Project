9481aa26b7f9936629ed9175dac9bdf8c230138d
RakeshKumarParida <rakeshkumarparida424@gmail.com>
2022-02-28 18:57:12 +0530

    Added ProductDetails page

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ if (selectAll) {
     navBar.classList.remove("active");
   });
 }
+
+
+
+// Product Details Code
+
+
+let mainImg = document.getElementById("mainImg");
+let smallImg = document.getElementsByClassName("sm-img");
+
+smallImg[0].addEventListener("click", () => {
+  mainImg.src = smallImg[0].src;
+});
+
+smallImg[1].addEventListener("click", () => {
+  mainImg.src = smallImg[1].src;
+});
+
+smallImg[2].addEventListener("click", () => {
+  mainImg.src = smallImg[2].src;
+});
+
+smallImg[3].addEventListener("click", () => {
+  mainImg.src = smallImg[3].src;
+});
